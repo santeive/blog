e180274e887fe12ae86b86dc06d627ce0e3829eb
santeive <packo.ive@gmail.com>
2020-07-07 17:27:35 -0500

    Comments module done

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from .models import Comment
 
+# Conventional form
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
-# Este form es dinamico
+# Dynamic form
+# We created a form from a Model
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
