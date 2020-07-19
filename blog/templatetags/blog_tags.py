e6d5e440bcbdf9f73ec5b703dd436c6b146bc1b8
santeive <packo.ive@gmail.com>
2020-07-18 22:46:11 -0500

    Filters and markdown done

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,5 @@
+from django.utils.safestring import mark_safe
+import markdown
 from django import template
 from django.db.models import Count
 from ..models import Post, Comment
@@ -18,3 +20,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
                total_comments=Count('comments')
            ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
